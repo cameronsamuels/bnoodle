4e224f16ee8c25146da68ed2e33e6d363d43ac0f
Cameron Samuels <me@cameronsamuels.com>
2017-09-25 16:41:58 -0500

    Revamped a lot
    - Added a meta description
    - Moved instructions outside of #s
    
    - Added comments to the js
    - Beautified the js
    - Cleaned up js
    - Removed duplicate declarations
    - Combined duplicate notation sets
    - Used addEventListener
    - Removed window from certain statements
    - Removed break from switch
    - Removed default case in switch
    - Removed duplicate while condition
    - Combined useragent match
    - Did not add touchevents on desktop
    - Used document instead of document.body
    
    - Gave less properties to #s in css
    - Made overflow hidden
    - Fixed #s being off the page
    - Removed hr's and replaced with borders (causing not full width dividers)
    - Adding padding to scrambles
    - Lessened margin of instructions
    - Fixed css spacing
    - Made border of dropdown be thicker

--- script.js
+++ script.js
@@ -1,59 +1,118 @@
-var scrambles = document.querySelector('#s'), cube = "3x3";
-    
-    function generateNotation() {
+// declare scramble div
+var scrambles = document.querySelector('#s'),
+// declare selected cube
+cube = "3x3",
+// declare the notations array
+n,
+// declare the add on arrays
+a,
+// declare current scramble index
+current = 0,
+// set max amount of scrambles
+max = Math.round(innerHeight / 100),
+// set scramble amounts for each cube
+amounts = {
+    "3x3": 0.8,
+    "2x2": 1.6,
+    "4x4": 0.4,
+    "5x5": 0.3,
+    "Pyraminx": 1.6,
+    "Skewb": 1.6
+},
+// set length of scrambles for each cube
+scrambleLengths = {
+    // 3x3 has 25 notation long scrambles
+    '3x3': 25,
+    // 2x2 has 9 notation long scrambles
+    '2x2': 9,
+    // 4x4 has 40 notation long scrambles
+    '4x4': 40,
+    // 5x5 has 60 notation long scrambles
+    '5x5': 60,
+    // Skewb has between 9 and 6 notation long scrambles
+    'Skewb': Math.round(Math.max(Math.random()*9,6)),
+    // Pyraminx has between 12 and 8 notation long scrambles
+    'Pyraminx': Math.round(Math.max(Math.random()*12,8))
+};
+// generate a single notation
+function generateNotation() {
+    // generate notation for the selected cube
     switch (cube) {
+        // for 3x3 and 2x2
         case "3x3" || "2x2":
+            // set usable sides
             n = ["U", "D", "F", "B", "L", "R"],
+            // set usable directions / add ons
             a = ["i", "i", "i", "2", "2", "", "", "", "", "", "", ""];
-            break;
-        case "Pyraminx":
-            n = ["U", "B", "L", "R"],
-            a = ["i", "i", "", "", ""];
-            break;
-        case "4x4":
+        // for 4x4 or 5x5
+        case "4x4" || "5x5":
+            // set usable sides
             n = ["U", "D", "F", "B", "L", "R", "Fw", "Bw", "Rw", "Lw", "Uw", "Bw"],
+            // set usable directions / add ons
             a = ["'", "'", "'", "2", "2", "", "", "", "", "", "", ""];
-            break;
-        case "5x5":
-            n = ["U", "D", "F", "B", "L", "R", "Fw", "Bw", "Rw", "Lw", "Uw", "Bw"],
-            a = ["'", "'", "'", "2", "2", "", "", "", "", "", "", ""];
-            break;
-        case "Skewb":
+        // for Pyraminx or Skewb
+        case "Pyraminx" || "Skewb":
+            // set usable sides
             n = ["U", "B", "L", "R"],
+            // set usable directions / add ons
             a = ["i", "i", "", "", ""];
-            break;
-        default:
-            n = ["U", "D", "F", "B", "L", "R"],
-            a = ["i", "i", "i", "2", "2", "", "", "", "", "", "", ""];
     }
+    // return notation value
     return n[Math.floor(n.length * Math.random())] + a[Math.floor(a.length * Math.random())]
 }
-var scrambleLengths = {}, a, n;
-scrambleLengths['3x3'] = 25, scrambleLengths['2x2'] = 9, scrambleLengths['4x4'] = 40, scrambleLengths['5x5'] = 60, scrambleLengths['Skewb'] = Math.round(Math.max(Math.random()*9,6)), scrambleLengths['Pyraminx'] = Math.round(Math.max(Math.random()*12,8));
-    
-    function generateScramble() {
-    scramble = '', notation = "U", notation2 = '', scrambleLengths['Pyraminx'] = Math.round(Math.max(Math.random()*12,8)), scrambleLengths['Skewb'] = Math.round(Math.max(Math.random()*9,6));
+// generate a whole entire scramble
+function generateScramble() {
+    // reset variables back to default values
+    scramble = '', notation = "U", notation2 = '',
+    // randomize lengths of Pyraminx and Skewb scrambles
+    scrambleLengths['Pyraminx'] = Math.round(Math.max(Math.random()*12,8)),
+    scrambleLengths['Skewb'] = Math.round(Math.max(Math.random()*9,6));
+    // generate a notation length times
     for (i = 0; i < scrambleLengths[cube]; i++) {
+        // set notation2 to the previous notation
         notation2 = notation;
+        // generate notation
         notation = generateNotation();
-        while (notation.charAt(0) == notation2.charAt(0) || Math.floor(n.indexOf(notation.charAt(0)) / 2) == Math.floor(n.indexOf(notation2.charAt(0)) / 2) || notation2.charAt(0) == notation.charAt(0)) {
+        // regenerate as long as the notation is not reasonable
+        while (
+            // if notation2 == notation
+            notation.charAt(0) == notation2.charAt(0)
+            // if notation has the opposite face as previous notation
+            || Math.floor(n.indexOf(notation.charAt(0)) / 2) == Math.floor(n.indexOf(notation2.charAt(0)) / 2) ) {
+            // generate notation
             notation = generateNotation();
         }
+        // Add the newly generated notation to the scramble variable
         scramble += notation + " ";
     }
+    // return the scramble value
     return scramble;
 }
-
-    
-    function displayScramble(){current>=max?(scrambles.innerHTML="<h2>Tap anywhere to generate a scramble</h2><hr /><h1>"+generateScramble()+"</h1>",current=1):current<max&&(scrambles.innerHTML+="<hr /><h1>"+generateScramble()+"</h1>",current++)}var current=0,max=Math.round(window.innerHeight/100),n,a;
-    
-    var m = {
-        "3x3":0.8,"2x2":1.6,"4x4":0.4,"5x5":0.3,"Pyraminx":1.6,"Skewb":1.6
-    }
-    function updateCube() {
-    cube = document.querySelector('#cube').value;
-    scrambles.innerHTML="<h2>Tap anywhere to generate a scramble</h2><hr /><h1>"+generateScramble()+"</h1>",current=1;
-    max = Math.round((window.innerHeight/100)*m[cube]);
+// generate and display a scramble
+function displayScramble() {
+    // if reached max scrambles, clear screen and generate & display another one
+    if (current >= max) scrambles.innerHTML = "<h1>" + generateScramble() + "</h1>", current = 1;
+    // if still have more scrambles, generate & display another one
+    else scrambles.innerHTML += "<h1>" + generateScramble() + "</h1>", current++;
 }
-    
-updateCube(),navigator.userAgent.match(/iPhone|iPad|iPod/i)||navigator.userAgent.match(/Android/i)||(scrambles.onclick=displayScramble),document.body.onresize=function(){max=Math.round((window.innerHeight/100)*m[cube])},document.body.ontouchmove=function(e){e.preventDefault()}
+// update selected cube
+function updateCube() {
+    // set current cube to dropdown value
+    cube = document.querySelector('#cube').value;
+    // generate and display scramble
+    scrambles.innerHTML = "<h1>" + generateScramble() + "</h1>",
+    // set current scramble to 1
+    current = 1;
+    // update scramble amount to generate
+    max = Math.round((innerHeight / 100) * amounts[cube]);
+}
+// update cube on load
+updateCube();
+// add touch events if mobile
+if (navigator.userAgent.match(/iPhone|iPad|iPod|Android/i)) scrambles.addEventListener('touchend', displayScramble);
+// add click events if desktop
+else scrambles.addEventListener('click', displayScramble);
+// dynamically fill the screen with scrambles based on window size
+document.addEventListener('resize', function() { max = Math.round((winnerHeight / 100) * amounts[cube]) }),
+// disable touch scrolling
+document.addEventListener('touchmove', function(e) { e.preventDefault() });
